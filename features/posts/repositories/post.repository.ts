7fcc9b3b715af9d371bf67e125c9f9caea5c5eeb
Chak Mahmud <muhammadshofyadi@gmail.com>
2026-08-01 14:26:32 +0700

    refactor post

---
+++ features/posts/repositories/post.repository.ts
@@ -0,0 +1,100 @@
+import { prisma } from "@/lib/prisma";
+
+export interface GetPostsParams {
+  search?: string;
+}
+
+export async function getPosts(params?: GetPostsParams) {
+  const search = params?.search?.trim();
+
+  return prisma.post.findMany({
+    where: search
+      ? {
+          OR: [
+            {
+              title: {
+                contains: search,
+                mode: "insensitive",
+              },
+            },
+            {
+              url: {
+                contains: search,
+                mode: "insensitive",
+              },
+            },
+            {
+              mainKeyword: {
+                contains: search,
+                mode: "insensitive",
+              },
+            },
+          ],
+        }
+      : undefined,
+
+    include: {
+      _count: {
+        select: {
+          pins: true,
+        },
+      },
+    },
+
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+}
+
+export async function getPostById(id: string) {
+  return prisma.post.findUnique({
+    where: {
+      id,
+    },
+
+    include: {
+      pins: true,
+    },
+  });
+}
+
+export async function createPost(data: {
+  url: string;
+  title: string;
+  mainKeyword?: string;
+  annotationKeywords?: string[];
+}) {
+  return prisma.post.create({
+    data: {
+      ...data,
+      annotationKeywords: data.annotationKeywords ?? [],
+    },
+  });
+}
+
+export async function updatePost(
+  id: string,
+  data: {
+    url: string;
+    title: string;
+    mainKeyword?: string;
+    annotationKeywords?: string[];
+  },
+) {
+  return prisma.post.update({
+    where: { id },
+    data: {
+      ...data,
+      annotationKeywords: data.annotationKeywords ?? [],
+    },
+  });
+}
+
+export async function deletePost(id: string) {
+  return prisma.post.delete({
+    where: {
+      id,
+    },
+  });
+}
